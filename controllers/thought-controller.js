041f326790a9525efcd77bc901fc1e8a3639cd69
Andrew Faugno <andrewfaugno825@gmail.com>
2022-07-30 02:41:56 -0400

    routes for creating and deleting reactions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -6,7 +6,7 @@ const thoughtController = {
         Thought.find()
             .populate({
                 path: 'reactions',
-                select: '-__v'
+                select: '-__v',
             })
             .select('-__v')
             .then(dbThoughtData => res.json(dbThoughtData))
@@ -80,6 +80,33 @@ const thoughtController = {
                 res.json(dbThoughtData);
             })
             .catch(err => res.json(err))
+    },
+
+    // create reaction
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: body } },
+            { new: true, runValidators: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                return res.status(404).json({ message: 'No Thought found with that id!' });
+            }
+            res.json(dbThoughtData)
+        })
+        .catch(err => res.json(err));
+    },
+
+    // delete reaction
+    removeReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: params.reactionId } } },
+            { new: true }
+        )
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => res.json);
     }
 };
 
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -4,23 +4,32 @@ const {
     getThoughtById,
     createThought,
     updateThought,
-    deleteThought
+    deleteThought,
+    addReaction,
+    removeReaction
 } = require('../../controllers/thought-controller');
 
-//    /api/thoughts
+//    GET all and Create thought
 router
     .route('/')
     .get(getAllThought)
-    .post(createThought)
+    .post(createThought);
 
-//    /api/thoughts/:thoughtId
+//    GET one and Update one thought
 router
     .route('/:thoughtId')
     .get(getThoughtById)
-    .put(updateThought)
+    .put(updateThought);
 
+//    delete a thought
 router
     .route('/:thoughtId/:userId')
-    .delete(deleteThought)
+    .delete(deleteThought);
+
+//    Create a reaction to a thought
+router.route('/:thoughtId/reactions').post(addReaction);
+
+//    Delete a reaction from a thought
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
